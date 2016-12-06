e48d0caf3ea92f18eaecb04a8f133538116d5cc8
Dhiraj Sapkal <dhirajsapkal@gmail.com>
2016-12-06 14:51:47 -0500

    Front end for home page done, basic scafolding files created, now we add php. Let the fun begin

---
+++ index.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+
+
+    
+    <div class="navbar">
+      <div class="row">
+        <div class="small-2 medium-3 large-2 columns">
+          <button class="my-button menu-button"><img src="img/menu.png" class="menu-icon"><span class="show-for-medium">Menu</span></button>
+        </div>
+        <div class="small-8 medium-6 large-8 columns">
+          <input class="input-box search-box" type="text" name="search">
+        </div>
+        <div class="small-2 medium-3 large-2 columns">
+          <button class="my-button account-button"><span class="show-for-medium">My account</span></button>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="row">
+      <div class="hero">
+        <div class="small-12 medium-12 columns">
+          <img src="img/macbook.jpg">
+        </div>
+        <div class="small-12 large-4 medium-6 medium-offset-4 end columns">
+          <div class="hero-transform">
+            <h2>15” MACBOOK PRO</h2>
+            <p>It’s faster and more powerful than before, yet remarkably thinner and lighter. It has the brightest, most colorful Mac notebook display ever. And it introduces the Touch Bar</p>
+            <button class="my-button action-button"><span>Starting at $1,299</span></button>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+
+      
+    <div class="row">
+      <div class="small-12 columns">
+        <!-- <div class="owl-carousel owl-theme">
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+          <div> Your Content </div>
+        </div> -->
+        <div class="card">
+          <div class="top-strip">
+            <a href="#"><h2 class="card-title">Electronics</h2></a>
+            <a href="#" class="card-view">View all</a>
+          </div>
+          <div class="product-slider">
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="slider-image">
+                <img src="img/mac.jpg">
+              </div>
+              <p>Title</p>
+              <div class="seperator"></div>
+              <div class="product-bottom">
+                <h3>Price</h3>
+                <button class="my-button">View</button>
+              </div>
+            </div>
+          </div>
+          <div class="slider-buttons">
+            <button type="button" class="my-button slider-previous"><img src="img/previous.png"></button>
+            <button type="button" class="my-button slider-next"><img src="img/next.png"></button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="row">
+        <div class="small-12 columns">
+          <p>Check out Admin mode!</p>
+        </div>
+      </div>
+    </div>
+
+
+
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/what-input/what-input.js"></script>
+    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/app.js"></script>
+
+    <div id="destroy"></div>
+  </body>
+</html>
