c421689f123c9d2f1ad529fec6be9a1fb79164d5
Dhiraj Sapkal <dhirajsapkal@gmail.com>
2016-12-17 00:03:13 +0530

    removed all bower files out

--- script-scaffold.php
+++ script-scaffold.php
@@ -1,6 +1,5 @@
 <script src="js/jquery.min.js"></script>
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>s -->
-<script src="bower_components/what-input/what-input.js"></script>
-<script src="bower_components/foundation-sites/dist/foundation.js"></script>
-<script src="js/app.js"></script>
+<script src="what-input/what-input.js"></script>
+<script src="foundation-sites/dist/foundation.js"></script>
 <script src="js/app.js"></script>
