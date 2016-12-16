aec28e05c6fa103d5a5900225bf8a21b1e522888
Dhiraj Sapkal <dhirajsapkal@gmail.com>
2016-12-17 00:07:59 +0530

    js

--- script-scaffold.php
+++ script-scaffold.php
@@ -1,5 +1,5 @@
 <script src="js/jquery.min.js"></script>
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>s -->
-<script src="what-input/what-input.js"></script>
-<script src="foundation-sites/dist/foundation.js"></script>
+<script src="js/what-input/what-input.js"></script>
+<script src="js/foundation-sites/dist/foundation.js"></script>
 <script src="js/app.js"></script>
