ca0161188ac4369b9505f00c71beca05edf4c634
Dhiraj Sapkal <dhirajsapkal@gmail.com>
2016-12-16 23:58:13 +0530

    Moved jquery out

--- script-scaffold.php
+++ script-scaffold.php
@@ -1,4 +1,6 @@
-<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="js/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>s -->
 <script src="bower_components/what-input/what-input.js"></script>
 <script src="bower_components/foundation-sites/dist/foundation.js"></script>
+<script src="js/app.js"></script>
 <script src="js/app.js"></script>
